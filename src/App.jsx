038e2cce39d0c390b83e28d9cae94040e425133b
angelooz <angelo-mjz7@hotmail.com>
2023-09-21 01:28:16 -0500

    add jessy

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,28 @@ import "./App.css";
 import { Flor } from "./components/Flor/flor";
 
 function App() {
-  const [nombre, setNombre] = useState("");
   const audio = useRef(null);
+  const [isClicked, setIsClicked] = useState(false);
 
-  useEffect(() => {
-    let queryString = window.location.search;
-    let params = new URLSearchParams(queryString);
-    const nombre = params.get("nombre");
-    setNombre(nombre);
-  }, []);
-
-  useEffect(() => {
+  const handleClick = async () => {
     if (audio.current) {
-      audio.current.play();
-      audio.current.volume = 0.2;
+      await audio.current.play();
+      audio.current.volume = 0.1;
     }
-  }, [audio]);
+    setIsClicked(true);
+  };
 
   return (
     <>
       <main className="main">
-        <h1 className="title">Para ✨{nombre}✨</h1>
+        <button
+          type="button"
+          className={`button ${isClicked ? "hidden" : ""}`}
+          onClick={handleClick}
+        >
+          Click me
+        </button>
+        <h1 className="title">✨Para mi Preciosa bby❤️✨</h1>
         <div className="container_flores">
           <Flor hideLeaf className="flor1" />
           <Flor hideLeaf2 hideLeaf className="flor2" />
@@ -32,11 +33,11 @@ function App() {
           <Flor hideLeaf2 className="flor4" />
         </div>
         <p className="parrafo">
-          🌼 ¡Hola! Espero que en tu camino hoy, encuentres tantas flores
-          amarillas 🌻 como las que Floricienta tenía en sus sueños. Que cada
-          una de esas flores te traiga alegría 😊, esperanza 🌟 y amor ❤️.
-          ¡Deseo que tengas un día mágico ✨ y lleno de encanto, al mejor estilo
-          de Floricienta! ¡Feliz día! 🌈🌼
+          🌼 ¡Hola! mi hermosa enojonita 😘😘 espero te gusten las flores
+          amarillas virtuales🌻. Que cada una de esas flores te traiga alegría
+          😊, esperanza 🌟 y amor ❤️. ¡Deseo que tengas un día mágico ✨ y lleno
+          de encanto, al mejor estilo de Floricienta! ¡Feliz día bebita! Te amo
+          ❤️❤️🌈🌼
         </p>
       </main>
       <audio ref={audio} src="audio/flores2.mp3" />
